51c43c37351655905c9b50793081f8eba6f55b35
venuaravind-14 <venu14092006@gmail.com>
2026-04-14 14:02:47 +0530

    changes

--- vaultx-frontend/vite.config.js
+++ vaultx-frontend/vite.config.js
@@ -2,10 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
+  preview: {
+    port: 4173,
+  },
+  server: {
+    historyApiFallback: true,
+  },
 })
